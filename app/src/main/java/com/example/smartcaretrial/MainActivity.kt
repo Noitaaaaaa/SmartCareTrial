5a94d61a55d0bc9c061d64de4b799c2f744e762f
Noitaaaaaa <noita1525@gmail.com>
2026-09-01 12:51:38 +0800

    Ugh

--- app/src/main/java/com/example/smartcaretrial/MainActivity.kt
+++ app/src/main/java/com/example/smartcaretrial/MainActivity.kt
@@ -28,7 +28,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -41,7 +40,6 @@ import com.example.smartcaretrial.ui.theme.SmartCareTrialTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             SmartCareTrialTheme {
