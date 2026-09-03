c116048d946a199d886c4ba34098d53c38f318d5
D4rkfall <jpmolera99@gmail.com>
2026-09-03 12:41:57 +0800

    Push mo yan teh

--- app/src/main/java/com/example/smartcaretrial/profile.kt
+++ app/src/main/java/com/example/smartcaretrial/profile.kt
@@ -1,14 +1,21 @@
 package com.example.smartcaretrial
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -29,7 +36,25 @@ fun Profile(
             .fillMaxWidth()
             .padding(16.dp)
     ) {
-        Text(text = "My Profile", style = MaterialTheme.typography.headlineSmall)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top
+        ) {
+            Text(
+                text = "My Profile",
+                style = MaterialTheme.typography.headlineSmall,
+                
+            )
+
+            Button(
+                onClick = { },
+            ) {
+                Text("Profile Picture of Doctor here")
+
+            }
+        }
+
 
         Spacer(modifier = Modifier.height(16.dp))
 
